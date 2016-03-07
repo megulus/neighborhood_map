fc4414ce7204fae28d6fee15b5e135be8bd94ddc
Meg Dahlgren <meg.dahlgren@gmail.com>
2016-03-07 16:33:57 -0500

    Load google maps API asynchronously and fix error message.

--- js/app.js
+++ js/app.js
@@ -1,21 +1,4 @@
-// Initialize Google Map
-var map;
 
-var initMap = function () {
-
-    var mapOptions = {
-        center: {lat: 40.663179, lng: -73.969167},
-        zoom: 12
-    };
-
-    map = new google.maps.Map(document.getElementById('map'), mapOptions);
-
-
-};
-
-var googleError = function () {
-    $('#map').text('Unable to load Google Maps');
-};
 
 
 
